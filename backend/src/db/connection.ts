40d02e20b80792edcfd387d3e7927b8a39b74f2b
jaydenpiao <jaydenpiao@gmail.com>
2023-12-03 22:14:29 -0800

    created db and restructuring

---
+++ backend/src/db/connection.ts
@@ -0,0 +1,8 @@
+import { connect } from "mongoose";
+async function connectToDatabase() {
+    try {
+        await connect(process.env.MONGODB_URL);
+    } catch (error) {
+        throw new Error("Cannot connect to MongoDB");
+    }
+}
