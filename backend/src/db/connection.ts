69826c0abbeb3d34416fbb4648b48d016b1c2c37
jaydenpiao <jaydenpiao@gmail.com>
2023-12-03 22:33:09 -0800

    db connect/disconnect

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,8 +1,21 @@
-import { connect } from "mongoose";
+import { connect, disconnect } from "mongoose";
 async function connectToDatabase() {
     try {
         await connect(process.env.MONGODB_URL);
     } catch (error) {
+        console.log(error);
         throw new Error("Cannot connect to MongoDB");
     }
 }
+
+async function disconnectFromDatabase() {
+    try {
+        await disconnect();
+    } catch (error) {
+        console.log(error);
+        throw new Error("Cannot disconnect from MongoDB");
+    }
+    
+}
+
+export { connectToDatabase, disconnectFromDatabase };
