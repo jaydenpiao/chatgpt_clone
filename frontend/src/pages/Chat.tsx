a6c33f970a50626209fabd2c662a7f8fa7244394
jaydenpiao <jaydenpiao@gmail.com>
2023-12-06 01:27:09 -0800

    input box

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,8 +1,9 @@
-import { Avatar, Box, Button, Typography } from '@mui/material'
+import { Avatar, Box, Button, IconButton, Typography } from '@mui/material'
 import React from 'react'
 import { useAuth } from '../context/AuthContext';
 import { red } from '@mui/material/colors';
 import ChatItem from '../components/chat/ChatItem';
+import { IoMdSend } from 'react-icons/io';
 
 const chatMessages = [
   {role: "user", content: "Hello, can you help me with my math homework?"},
@@ -45,6 +46,15 @@ const Chat = () => {
         <Box sx={{width:'100%', height:'60vh', borderRadius:3, mx:'auto', display:'flex', flexDirection:'column', overflow:'scroll', overflowX: 'hidden', overflowY:'auto', scrollBehavior:'smooth',}}>
           {chatMessages.map((chat, index) => (<ChatItem content={chat.content} role={chat.role} key={index} />))}
         </Box>
+        <div style={{width: '100%', padding:'20px', borderRadius:8, backgroundColor:'rgb(17,27,39)', display:'flex', marginRight:'auto',}}>
+          {" "}
+          <input type='text' style={{width:'100%', backgroundColor:'transparent', padding:'10px', border:'none', outline:'none', color:'white', fontSize:'20px',}} />
+          
+          <IconButton sx={{ml:'auto', color:'white', }}>
+            <IoMdSend />
+          </IconButton>
+        </div>
+        
 
       </Box>
     </Box>
