2ac181e8aaca02e30a58fdb790a2e2f414b2a875
jaydenpiao <jaydenpiao@gmail.com>
2023-12-05 19:35:23 -0800

    verifies user w/ respective token

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import appRouter from "./index.js";
-import { getAllUsers, userLogin, userSignup } from "../controllers/user-controllers.js";
+import { getAllUsers, userLogin, userSignup, verifyUser } from "../controllers/user-controllers.js";
 import { loginValidator, signupValidator, validate } from "../utils/validators.js";
 import { verifyToken } from "../utils/token-manager.js";
 
@@ -9,6 +9,6 @@ const userRoutes = Router();
 userRoutes.get("/", getAllUsers);
 userRoutes.post("/signup", validate(signupValidator), userSignup);
 userRoutes.post("/login", validate(loginValidator), userLogin);
-userRoutes.get("/auth-status", verifyToken, userLogin);
+userRoutes.get("/auth-status", verifyToken, verifyUser);
 
 export default userRoutes;
